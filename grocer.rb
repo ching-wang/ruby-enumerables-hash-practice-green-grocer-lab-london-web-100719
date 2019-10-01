911192d9ed9b722afd1d9aac4cc557fe00251b0a
Qing Wang <chingwang.qw@gmail.com>
2019-10-01 20:05:25 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -24,6 +24,7 @@ def apply_coupons(cart, coupons)
         cart[coupon_item][:count] -= coupon[:num]
         new_key = "#{coupon_item} W/COUPON" 
         if cart[new_key]
+           cart[new_key][:count] += coupon[:num]
         end 
       end
     end 
